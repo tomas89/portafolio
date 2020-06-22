d59997b66c18962e1ee0ac71814ae619a4cc65ef
tfbm89 <tfbm89@gmail.com>
2020-06-21 23:23:28 -0500

    Se agregarron algunos Skills

--- gulpfile.js
+++ gulpfile.js
@@ -2,27 +2,16 @@ const gulp = require ('gulp');
 
 const sass = require ('gulp-sass');
 
-const autoprefixer = require ('gulp-autoprefixer');
+sass.compiler = require('node-sass');
 
 
-gulp.task('sass', ()=>{
+gulp.task('sass', function () {
+    return gulp.src('./sass/**/*.scss')
+      .pipe(sass().on('error', sass.logError))
+      .pipe(gulp.dest('./css'));
+  });
 
-    gulp.src('./scss/*')
-        .pipe(sass({
-            outputStyle:'compact'
-        }))
-        .pipe(autoprefixer({
-            browsers:['last 3 versions']
-        }))
-        .pipe(gulp.dest('./css'))
-
-
-});
-
-
-
-gulp.task('t', ()=> {
-    console.log('This is a homework');
-    console.log('My test');
-} );
-
+  
+  gulp.task('sass:watch', function () {
+    gulp.watch('./sass/**/*.scss', ['sass']);
+  });
